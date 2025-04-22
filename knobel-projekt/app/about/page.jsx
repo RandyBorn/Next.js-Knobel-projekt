3e78258070455ccdfc349b7fa1097b64d2663c97
RandyBorn <randyborn677@yahoo.com>
2025-04-22 20:30:16 +0200

    projekt-optimirung-tailwind

--- knobel-projekt/app/about/page.jsx
+++ knobel-projekt/app/about/page.jsx
@@ -1,3 +1,4 @@
+import Navbar from "@/components/Navbar";
 import React from "react";
 // import WindowResizeLogger from "@/components/WindowResizeLogger";
 // import UserInfoForm from "@/components/UserInfoForm";
@@ -5,21 +6,35 @@ import React from "react";
 function AboutPage() {
   return (
     <>
-      <div className="aboutbody">
-        <div className="aboutfirst">
-          <h3>The size of the Screen could be important...</h3>
-          <div className="aboutfirstin">{/* <WindowResizeLogger /> */}</div>
+      <Navbar />
+      <section className="aboutpage border-2 border-b-blue-100 bg-blue-50 display flex flex-col justify-center items-center">
+        <h3>The size of the Screen could be important...</h3>
+        <div className="aboutbody  ">
+          <section className="aboutsection md:bg-blue-500 text-red-700 m-6 border-8 border-b-blue-800-300">
+            <h2>About</h2>
+            <p>
+              width: 786px
+              <br />
+              screen.width: 786px
+              <br />
+            </p>
+          </section>
+          <div className="aboutfirst">
+            <div className="aboutfirstin">{/* <WindowResizeLogger /> */}</div>
+          </div>
+          <div className="aboutsecond">{/* <UserInfoForm /> */}</div>
+          <div>
+            <section className="aboutsection text-red-700 m-6 border-8 border-b-blue-800-300">
+              <p>Anzahl logischer Prozessoren: 8</p>
+              <p>Ram: 8GB</p>
+              <p>
+                Browser nutzen oft nur einen fest zugeteilten Teil des Rams aus
+                Desktop PC´s sind das oft max. 8GB
+              </p>
+            </section>
+          </div>
         </div>
-        <div className="aboutsecond">{/* <UserInfoForm /> */}</div>
-        <div className="aboutthird">
-          <p>Anzahl logischer Prozessoren: 8</p>
-          <p>Ram: 8GB</p>
-          <p>
-            Browser nutzen oft nur einen fest zugeteilten Teil des Rams aus
-            Desktop PC´s sind das oft max. 8GB
-          </p>
-        </div>
-      </div>
+      </section>
     </>
   );
 }
